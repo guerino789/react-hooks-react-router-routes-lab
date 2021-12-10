34d867d952e0d61ef8dfcd453bf1be5e961827cc
guerino <guerantonini94@gmail.com>
2021-12-10 18:43:36 -0500

    complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayDirector = directors.map((director) => (
+    <div key={director.name}>
+      {director.name}
+      {director.movies.map((movie) => (
+        <ul key={movie}>
+          <li>{movie}</li>
+        </ul>
+      ))}
+    </div>
+
+  ))
+
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {displayDirector}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayMovie = movies.map((movie) => (
+    <div key={movie.title}>
+      <h1>{movie.title}</h1>
+      <p>{movie.time}</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovie}
+    </div>
+
+  )
 }
 
 export default Movies;
